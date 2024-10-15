cd2cf2da35fa6bbe9dfacabfb2304b87dcafbbe8
Katarina Krivokapic <katarina03kk@gmail.com>
2024-10-15 18:21:17 -0500

    worked on Polygon.java

--- app/src/main/java/edu/luc/etl/cs313/android/shapes/model/Polygon.java
+++ app/src/main/java/edu/luc/etl/cs313/android/shapes/model/Polygon.java
@@ -20,6 +20,7 @@ public class Polygon extends Group {
     @Override
     public <Result> Result accept(final Visitor<Result> v) {
         // TODO your job
-        return null;
+        // to be tested
+        return v.onPolygon(this); // calls onPolygon from interface
     }
 }
