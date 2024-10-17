0d276459138ca82b0b646e6e2036f7966bd9eca6
dgodinez44 <dgodinez1@luc.edu>
2024-10-17 18:22:09 -0500

    worked on DrawWidget

--- app/src/main/java/edu/luc/etl/cs313/android/shapes/android/DrawWidget.java
+++ app/src/main/java/edu/luc/etl/cs313/android/shapes/android/DrawWidget.java
@@ -57,19 +57,29 @@ public class DrawWidget extends View {
         // Draw the red rectangle (right side)
         paint.setColor(Color.RED);
         paint.setStyle(Paint.Style.FILL);
-        canvas.drawRect(50, 10, 70, 30, paint);
+        canvas.drawRect(45, 10, 60, 20, paint);
         paint.setStyle(Paint.Style.STROKE);
-        canvas.drawRect(50, 10, 90, 50, paint);
+        canvas.drawRect(45, 10, 100, 40, paint);
 
         // Draw the blue polygon (trapezoid)
         paint.setColor(Color.BLUE);
-        float[] trapezoidPts = {
-                55, 35,  // First point
-                65, 45,  // Second point
-                80, 45,  // Third point
-                85, 35,  // Fourth point
-                55, 35   // Back to first point
-        };
+        Polygon trapezoid = new Polygon(
+                new Point(55, 35),
+                new Point(65, 45),
+                new Point(80, 45),
+                new Point(85, 35)
+        );
+
+        float[] trapezoidPts = new float[trapezoid.getPoints().size() * 4];
+        int i = 0;
+        for (int j = 0; j < trapezoid.getPoints().size(); j++) {
+            Point current = trapezoid.getPoints().get(j);
+            Point next = trapezoid.getPoints().get((j + 1) % trapezoid.getPoints().size());
+            trapezoidPts[i++] = current.getX();
+            trapezoidPts[i++] = current.getY();
+            trapezoidPts[i++] = next.getX();
+            trapezoidPts[i++] = next.getY();
+        }
         canvas.drawLines(trapezoidPts, paint);
 
         // Draw the purple circle
@@ -78,12 +88,29 @@ public class DrawWidget extends View {
 
         // Draw the partial octagon (top left)
         paint.setColor(Color.BLACK);
-        float[] octagonPts = {
-                10, 10,  // First point
-                20, 5,   // Second point
-                30, 10,  // Third point
-                10, 10   // Back to first point
-        };
+        Polygon octagon = new Polygon(
+                //new Point(10, 10),
+                //new Point(20, 5),
+                //new Point(30, 10)
+                new Point(10, 15),  // Top
+                new Point(15, 10),  // Top-right
+                new Point(25, 10),  // Right
+                new Point(30, 15),  // Bottom-right
+                new Point(30, 25),  // Bottom
+                new Point(25, 30),  // Bottom-left
+                new Point(15, 30),  // Left
+                new Point(10, 25)   // Top-left
+        );
+        float[] octagonPts = new float[octagon.getPoints().size() * 4];
+        i = 0;
+        for (int j = 0; j < octagon.getPoints().size(); j++) {
+            Point current = octagon.getPoints().get(j);
+            Point next = octagon.getPoints().get((j + 1) % octagon.getPoints().size());
+            octagonPts[i++] = current.getX();
+            octagonPts[i++] = current.getY();
+            octagonPts[i++] = next.getX();
+            octagonPts[i++] = next.getY();
+        }
         canvas.drawLines(octagonPts, paint);
 
     }
